f4db7356681f7640acd5020d5172111323b5bd1c
Matthew McRaven <mkm302@georgetown.edu>
2020-04-04 17:07:29 -0700

    Add helper function to perform tensor padding.

--- utils.py
+++ utils.py
@@ -16,6 +16,16 @@ def cuda(arr, config):
 def is_power2(number):
 	return number > 0 and math.ceil(math.log(number, 2)) == math.floor(math.log(number, 2)) 
 
+# Return the next power of two larger than number, and the number of indices needed padding above and below the number.
+def pad_nearest_pow2(number):
+	next_pow2 = number
+	pad_first, pad_second = 0,0
+	if not is_power2(number):
+		next_pow2 = 2**math.ceil(math.log(number, 2))
+		needed_padding = next_pow2 - number
+		pad_first = needed_padding // 2
+		pad_second = needed_padding - pad_first
+	return (next_pow2, pad_first, pad_second)
 # Convert images to tensors, and apply normalization if necessary
 def image_to_tensor(image):
 	#TODO: apply image normalization.
