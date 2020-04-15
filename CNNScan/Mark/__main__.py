ac0e9707da8864a36775eb4d20da2e9234b792e3
Matthew McRaven <mkm302@georgetown.edu>
2020-04-14 17:53:13 -0700

    Load marked ballots from disk.
    Complete the DirectoryDataSet class, which enables loading previously marked ballots from the disk.
    This allows re-using a dataset between learning runs.

--- CNNScan/Mark/__main__.py
+++ CNNScan/Mark/__main__.py
@@ -12,11 +12,12 @@ import CNNScan.Raster
 def main(parser):
 	options = parser.parse_args()
 	# Determine output directory
-	print(options.outdir)
 	output_base_path = os.path.abspath(options.outdir[0])
 	# Determine number of images / marking conditions
-
+	count = options.count
+	# Load the ballot definition
 	ballot = CNNScan.Samples.Oregon.ballot
+	# Create output directory.
 	output_directory = output_base_path
 	if not os.path.exists(output_directory):
 		os.mkdir(output_directory)
@@ -31,27 +32,12 @@ def main(parser):
 	print(os.path.abspath(ballot.ballot_file))
 	ballot = CNNScan.Raster.Raster.rasterize_ballot_template(ballot, output_directory+"/ballot_template", 400)
 
+	# Determine marking parameters
+	mark_db = CNNScan.Mark.MarkDatabase()
+	mark_db.insert_mark(CNNScan.Mark.BoxMark())
 	# Create marked ballots
-	marked_ballots = CNNScan.Mark.mark_dataset(ballot,transforms, count=1000)
-	# TODO: use generator expression to create ballots in batches, to reduce memory pressure on host machine
-	with open(output_directory+"/ballot-template.p", "wb") as file:
-		pickle.dump(ballot, file)
-	for i in range(len(marked_ballots)):
-		marked_ballot = marked_ballots.at(i)
-		print(marked_ballot)
-		# Serialize all contests to PNG's so they may be inspected.
-		# Null out reference to image, so it does not get pickled.
-		ballot_dir = output_directory+"/ballot%s/" %i
-		if not os.path.exists(ballot_dir):
-			os.mkdir(ballot_dir)
-		for j, contest in enumerate(marked_ballot.marked_contest):
-			print(ballot_dir+f"c{j:06}.png")
-			contest.image.save(ballot_dir+f"c{j:06}.png")
-			contest.clear_data()
-			
-		# Serialize ballot (without images!) to object in directory structure
-		with open(ballot_dir+f"ballot{i}.p", "wb") as file:
-			pickle.dump(marked_ballot, file)
+	marked_ballots = CNNScan.Reco.Load.GeneratingDataSet(ballot, mark_db, count, transforms, True )
+	marked_ballots.save_to_directory(output_directory)
 		
 		
 	# Open the directory for 
@@ -59,5 +45,5 @@ def main(parser):
 if __name__ == "__main__":
 	parser = argparse.ArgumentParser()
 	parser.add_argument("--outdir",help="Directory in which to store files", required=True, nargs=1)
-	parser.add_argument("--count", default=100, help="Directory in which to store files", required=True)
+	parser.add_argument("--count", default=100, help="Directory in which to store files", type=int)
 	main(parser)
--- CNNScan/Reco/Load.py
+++ CNNScan/Reco/Load.py
@@ -2,13 +2,15 @@ from typing import List
 import importlib.resources
 import random
 import pickle
+import os.path
+import re
 
-import torch
 import numpy as np
 import numpy.random
 import pylab as plt
-
+import torch
 from torch.utils.data import Dataset, DataLoader
+from PIL import Image
 
 import CNNScan.Ballot.MarkedBallots
 
@@ -41,6 +43,9 @@ def create_marked_ballot(ballot, mark_database):
 		contests.append(latest)
 	return CNNScan.Ballot.MarkedBallots.MarkedBallot(ballot, contests)
 
+# If lazy_nondeterminism is true, then ballots will only be loaded as they are referenced (a good option for files)
+# If it is false, then all ballots will be loaded into memory before __init__ returns, 
+# which is a good choice for generating ballots deterministically
 class BallotDataSet(Dataset):
 	def __init__(self, ballot_definition, ballot_count,  transforms, lazy_nondeterminism=True):
 		self.ballot_definition = ballot_definition
@@ -54,12 +59,15 @@ class BallotDataSet(Dataset):
 			for i in range(self.ballot_count):
 				self.at(i)
 
+	# Subclasses must override this in order to support lazy-loading of ballots
 	def load_ballot(self, index):
 		raise NotImplementedError
 
+	# Fetch the item at index, which may not yet be loaded into memory.
 	def at(self, index):
 		if self.marked_ballots[index] is None:
 			self.marked_ballots[index] = self.load_ballot(index)
+			# Tensors were purged on save, so they must be re-constructed.
 			for contest in self.marked_ballots[index].marked_contest:
 				contest.tensor = self.transforms(contest.image)
 		return self.marked_ballots[index]
@@ -77,22 +85,84 @@ class BallotDataSet(Dataset):
 
 	def __len__(self):
 		return self.ballot_count
+	
+	# Save all of the ballots/contests in the dataset to a file in a pre-determined fashion.
+	def save_to_directory(self, output_directory, print_images=True):
+		if not os.path.exists(output_directory):
+			os.mkdir(output_directory)
+		# TODO: use generator expression to create ballots in batches, to reduce memory pressure on host machine
+		with open(output_directory+"/ballot-definition.p", "wb") as file:
+			pickle.dump(self.ballot_definition, file)
+		for i in range(len(self.marked_ballots)):
+			marked_ballot = self.at(i)
+			print(marked_ballot)
+			# Serialize all contests to PNG's so they may be inspected.
+			# Null out reference to image, so it does not get pickled.
+			ballot_dir = self.ballot_dir(output_directory, i)
+			if not os.path.exists(ballot_dir):
+				os.mkdir(ballot_dir)
+				for j, contest in enumerate(marked_ballot.marked_contest):
+					print(ballot_dir+f"c{j}.png")
+					contest.image.save(ballot_dir+f"c{j}.png")
+					contest.clear_data()
+				
+			# Serialize ballot (without images!) to object in directory structure
+			with open(ballot_dir+self.ballot_name(i), "wb") as file:
+				pickle.dump(marked_ballot, file)
+
+	# Determine the directory where a ballot is stored.
+	def ballot_dir(self, directory, index):
+		return directory + "/ballot%s/" % index
+
+	# Determine the name for a contest's image
+	def ballot_name(self, index):
+		return f"ballot{index}.p"
 
 class DirectoryDataSet(BallotDataSet):
 	def __init__(self, directory, transforms, lazy_nondeterminism=True):
 		self.directory = directory
-		super(DirectoryDataSet, self).__init__(self.load_ballot_def(), self.count_ballots(), transforms, lazy_nondeterminism=lazy_nondeterminism)
+		super(DirectoryDataSet, self).__init__(self.load_ballot_definition(), self.count_ballots(), transforms, lazy_nondeterminism=lazy_nondeterminism)
 
+	# Load a marked ballot with a particular index.
 	def load_ballot(self, index):
 		# We shouldn't be asked to create a new ballot when one already exists
 		assert self.marked_ballots[index] is None
-		raise NotImplementedError("Can't yet read from directories")
+		directory = self.ballot_dir(self.directory, index)
+		assert os.path.exists(directory)
+		fp = self.ballot_name(index)
+		assert os.path.isfile(directory+fp)
+		with open(directory+fp, "rb") as file:
+			marked = pickle.load(file)
+			assert isinstance(marked, CNNScan.Ballot.MarkedBallots.MarkedBallot)
+			# TODO: Save entire pages of ballot, crop from entire page of ballot.
+			# Must re-load all the pictures for each contest.
+			for i, contest in enumerate(marked.marked_contest):
+				contest.image = Image.open(directory+f"c{i}.png")
+			#print(marked)
+			return marked
 
+	# Load and return the object containing the ballot definition.
 	def load_ballot_definition(self):
+		if os.path.exists(self.directory+"/ballot-definition.p"):
+			with open(self.directory+"/ballot-definition.p", "rb") as file:
+				template = pickle.load(file)
+				assert isinstance(template, CNNScan.Ballot.BallotDefinitions.Ballot)
+				#print(template)
+				return template
+		else:
+			raise ValueError(f"No ballot definition found at {self.directory}")
 		raise NotImplementedError("Can't yet read from directories")
 
+	# Figure out the number of marked ballot files in the current directory.
 	def count_ballots(self):
-		raise NotImplementedError("Can't yet read from directories")
+		dirs = os.listdir(path=self.directory)
+		count = 0
+		program = re.compile("ballot[0-9]+")
+		for x in dirs:
+			if program.match(x):
+				count += 1
+		#print(count)
+		return count
 
 
 class GeneratingDataSet(BallotDataSet):
