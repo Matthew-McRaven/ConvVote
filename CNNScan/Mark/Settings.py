5d6541545e9b047e760bb1a5b40616789f5fede5
Matthew McRaven <mkm302@georgetown.edu>
2020-05-02 17:16:37 -0700

    Create Auto-Encoder from GAN generator.
    After conversation with Dr. Garman, we decided to construct an auto-encoder that uses our gan as the decoder stage.
    This will be useful in diagnosing difficulties in our NN.
    Since our image quality is low, this will help us determine if the generator is bad or if the discriminator is bad.
    Or, it could determine that the tensor-to-image conversion doesn't work.

--- CNNScan/Mark/Settings.py
+++ CNNScan/Mark/Settings.py
@@ -2,30 +2,38 @@ import CNNScan.Settings# Create sensible defaults for all the settings in the ne
 def generate_default_settings():
 	ret = CNNScan.Settings.generate_default_settings()
 	ret['nlo']					= "lrelu"
-	ret['learning_rate'] 		= 0.00001
+	ret['learning_rate'] 		= 0.0002
 	# Size of mark images, mark outputs
 	ret['im_size']				= (2, 128, 128)
-	ret['criterion'] 			= 'bce-sum'
+	ret['criterion'] 			= 'mse-sum'
 	# How many random values should be used to seed the generator?
-	ret['gen_seed_len']			= 100
+	ret['gen_seed_len']			= 4096
 	# How many embedding entires should be used to encode class labels?
 	ret['gen_full_layers']		= []
 	# How should the CNN be seeded? (channels x H * W)
-	ret['gen_seed_image']		= (15, 16, 16)
+	ret['gen_seed_image']		= (8, 16, 16)
 	ret['gen_conv_layers'] 	= [
 		# Make sure the kernel size is SMALLER than the feature being recognized.
-		CNNScan.Settings.conv_transpose_def(4, 128,  2, 1, 1, 0, True),
-		CNNScan.Settings.conv_transpose_def(4, 64,  2, 1, 1, 0, True),
-		CNNScan.Settings.conv_transpose_def(5, 2,  2, 2, 1, 1, False),
+		CNNScan.Settings.conv_transpose_def(3, 64,  2, 1, 1, 1, True),
+		CNNScan.Settings.conv_transpose_def(3, 64,  2, 1, 1, 1, True),
+		CNNScan.Settings.conv_transpose_def(3, 2,  2, 1, 1, 1, False),
 	]
 
 	# Discriminator configuraiton
 	ret['disc_conv_layers'] 	= [
 		# Make sure the kernel size is SMALLER than the feature being recognized.
-		CNNScan.Settings.conv_def(2, 4, 1, 0, 1, False),
-		CNNScan.Settings.pool_def(4)
+		CNNScan.Settings.conv_def(3, 64, 2, 0, 1, False),
+		CNNScan.Settings.conv_def(3, 64, 2, 0, 1, False),
 	]
-	ret['disc_full_layers']		= [200]
+	ret['disc_full_layers']		= [600, 400]
+
+	# Autoencode params
+	ret['enc_conv_layers']		= [
+		# Make sure the kernel size is SMALLER than the feature being recognized.
+		CNNScan.Settings.conv_def(4, 64, 2, 0, 1, False),
+		CNNScan.Settings.conv_def(4, 16, 2, 0, 1, False),
+	]
+	ret['enc_full_layers']		= [8192*2, 8192]
 	
 
 	return ret
--- sweep_gan.py
+++ sweep_gan.py
@@ -14,58 +14,6 @@ import matplotlib
 from CNNScan.Mark import gan
 import CNNScan.Mark.Settings
 
-def raster_images(images, my_dir):
-	# Must use this line to prevent crashing.
-	matplotlib.use('agg')
-	# Convert a 2 channel tensor to an image.
-	asLA= torchvision.transforms.Compose([
-		torchvision.transforms.Normalize((-1/127.5,),(1/127.5,)),
-		torchvision.transforms.ToPILImage()
-	])
-	# Convert a 1 channel tensor to an image.
-	asL= torchvision.transforms.Compose([
-		torchvision.transforms.Normalize((-1/127.5,),(1/127.5,)),
-		torchvision.transforms.ToPILImage()
-	])
-	my_filter = ImageFilter.FIND_EDGES#ImageFilter.Kernel((3,3), [0,1,0,1,-4,1,0,1,0], scale=4)
-	# Perform channel decomposition and analysis on each of the generated images
-	for i,image in enumerate(images):
-		fig, ((og, l, a, la), (og_grad, l_grad, a_grad, la_grad)) = plt.subplots(2, 4)
-		fig.suptitle(f'Image {i} Channel Decomposition')
-
-		im1 = asLA(image)
-		RGBA = image[0]#(1/3)*image[0] + (1/3)*image[1] + (1/3)*image[2]
-		im2 = asL( torch.stack((RGBA,)) )
-		im3 = asL( torch.stack((image[1],))  )
-		im4 = asL( torch.stack((RGBA-image[1],)) )
-
-		# Add axis titles and images.
-		og.set_title("Original Image")
-		og.imshow(im1)
-		og_grad.set_title("Original Gradient")
-		og_grad.imshow(im1.filter(my_filter))
-
-		l.set_title("L Channel")
-		l.imshow(im2,cmap='Greys_r')
-		l_grad.set_title("L Gradient")
-		l_grad.imshow(im2.filter(my_filter),cmap='Greys')
-
-		a.set_title("A Channel")
-		a.imshow(im3,cmap='Greys_r')
-		a_grad.set_title("A Gradient")
-		a_grad.imshow(im3.filter(my_filter),cmap='Greys')
-
-		la.set_title("L-A Channel")
-		la.imshow(im4,cmap='Greys')
-		la_grad.set_title("L-A Gradient")
-		la_grad.imshow(im4.filter(my_filter),cmap='Greys')
-
-		for ax in fig.get_axes():
-			ax.label_outer()
-
-		fig.savefig(my_dir+f"/file{i}.png", dpi=400)
-
-
 def do_once(config):
 	transforms = torchvision.transforms.Compose([
 												#torchvision.transforms.Grayscale(),
@@ -87,7 +35,9 @@ def do_once(config):
 	my_dir = "."
 	with open(my_dir+"/settings.p", "wb") as my_file:
 		pickle.dump(config, my_file)
-	raster_images(images, my_dir)
+	# Must use this line to prevent crashing.
+	matplotlib.use('agg')
+	CNNScan.Mark.raster_images(images, my_dir)
 
 	del disc_model
 	del gen_model
@@ -104,33 +54,33 @@ def main():
 		from ray import tune
 		import ray
 		ray.init()
-		config['epochs'] = tune.grid_search([100, 200, 400])
+		config['epochs'] = tune.grid_search([0])
 		config['learning_rate'] = tune.grid_search([0.0001, 0.00001, 0.000001])
 		config['gen_seed_len'] = tune.grid_search([10, 50, 100, 200])
 		layers = []
 		layers.append([
-		# Make sure the kernel size is SMALLER than the feature being recognized.
-		CNNScan.Settings.conv_def(2, 4, 1, 0, 1, False),
-		CNNScan.Settings.pool_def(4)
+			# Make sure the kernel size is SMALLER than the feature being recognized.
+			CNNScan.Settings.conv_def(2, 4, 1, 0, 1, False),
+			CNNScan.Settings.pool_def(4)
 		])
 		layers.append([
-		# Make sure the kernel size is SMALLER than the feature being recognized.
-		CNNScan.Settings.conv_def(2, 16, 1, 0, 1, False),
-		CNNScan.Settings.pool_def(2)
+			# Make sure the kernel size is SMALLER than the feature being recognized.
+			CNNScan.Settings.conv_def(2, 16, 1, 0, 1, False),
+			CNNScan.Settings.pool_def(2)
 		])
 		layers.append([
-		# Make sure the kernel size is SMALLER than the feature being recognized.
-		CNNScan.Settings.conv_def(4, 16, 1, 0, 1, False),
-		CNNScan.Settings.conv_def(4, 16, 1, 0, 1, True),
-		CNNScan.Settings.pool_def(4)
+			# Make sure the kernel size is SMALLER than the feature being recognized.
+			CNNScan.Settings.conv_def(4, 16, 1, 0, 1, False),
+			CNNScan.Settings.conv_def(4, 16, 1, 0, 1, True),
+			CNNScan.Settings.pool_def(4)
 		])
 		layers.append([
-		# Make sure the kernel size is SMALLER than the feature being recognized.
-		CNNScan.Settings.conv_def(4, 32, 1, 0, 1, False),
-		CNNScan.Settings.conv_def(4, 32, 1, 0, 1, True),
-		CNNScan.Settings.pool_def(4),
-		CNNScan.Settings.conv_def(7, 16, 1, 0, 1, False),
-		CNNScan.Settings.conv_def(7, 16, 1, 0, 1, True),
+			# Make sure the kernel size is SMALLER than the feature being recognized.
+			CNNScan.Settings.conv_def(4, 32, 1, 0, 1, False),
+			CNNScan.Settings.conv_def(4, 32, 1, 0, 1, True),
+			CNNScan.Settings.pool_def(4),
+			CNNScan.Settings.conv_def(7, 16, 1, 0, 1, False),
+			CNNScan.Settings.conv_def(7, 16, 1, 0, 1, True),
 		])
 		config['disc_conv_layers'] = tune.grid_search(layers)
 		config['disc_full_layers'] = tune.grid_search([[200], [400,200], [800,400,200], [800,800,800]])
